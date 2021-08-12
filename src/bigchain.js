458c5e621639bc603a27ecb329d23e25b8818650
Premkumar Veerakumar <premchelliah@hotmail.com>
2021-08-12 14:09:33 +0530

    Update bigchain.js

--- src/bigchain.js
+++ src/bigchain.js
@@ -2,15 +2,18 @@ import driver from 'bigchaindb-driver';
 
 const API_PATH = 'http://192.168.50.94:9984/api/v1/';
 
-export async function putOrderRequest(user, order_input) {
-    const conn = new driver.Connection(API_PATH);
+export async function putOrderRequest(user, orderRequest) {
+    const conn = new driver.Connection(API_PATH),
+        orderNo = orderRequest.orderNo;
+    delete orderRequest.orderNo;
+    orderRequest.orderDate = new Date().toISOString();
     let result;
     try {
         result = await conn.postTransactionCommit(
             driver.Transaction.signTransaction(
                 driver.Transaction.makeCreateTransaction(
-                    order_input,
-                    null, // split metadata from order_input into this object
+                    { orderNo },
+                    { orderRequest }, // split metadata from order_input into this object
                     [
                         driver.Transaction.makeOutput(
                             driver.Transaction.makeEd25519Condition(
@@ -26,5 +29,5 @@ export async function putOrderRequest(user, order_input) {
     } catch (e) {
         console.error(JSON.stringify(e));
     }
-    return { id: result.id, ...result.asset.data };
+    return result.id;
 }
