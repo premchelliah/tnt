79f8d2ba8e52883ed8821afc542b4c6c979f6168
Premkumar Veerakumar <premchelliah@hotmail.com>
2021-08-13 11:08:22 +0530

    Update bigchain.js

--- src/bigchain.js
+++ src/bigchain.js
@@ -1,6 +1,6 @@
 import driver from 'bigchaindb-driver';
 
-const API_PATH = 'http://192.168.50.94:9984/api/v1/';
+const API_PATH = 'http://192.168.50.232:9984/api/v1/';
 
 export async function putOrderRequest(user, orderRequest) {
     const conn = new driver.Connection(API_PATH),
